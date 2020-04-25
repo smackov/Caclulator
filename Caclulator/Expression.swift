591f6591ad928f5c882391600123fd2e6bbb6b25
smackov <smackovdinislam@gmail.com>
2020-04-25 13:27:04 +0500

    Version 2.0 Using enums and new functions But the calculator don't work correctly

--- Caclulator/Expression.swift
+++ Caclulator/Expression.swift
@@ -8,58 +8,125 @@
 
 import Foundation
 
+enum Position {
+    case firstNumber, signOperator, secondNumber
+}
 
+enum OperatorSign {
+    case division, subtraction, multiplication, addition, none
+    
+    var text: String {
+        switch self {
+        case .division:
+            return "/"
+        case .subtraction:
+            return "-"
+        case .addition:
+            return "+"
+        case .multiplication:
+            return "*"
+        case .none:
+            return ""
+        }
+    }
+}
 
-struct Expression {
-    var firstValue = [Double]()
-    var secondValue = [Double]()
-    var operationSign: String
-    var currentPosition: Bool
+//struct Numbers {
+//    var number: String = ""
+//
+//    func append(_ character: String) {
+//        number.append(character)
+//    }
+//}
+
+struct Calculator {
+    var firstNumber: String = ""
+    var secondNumber: String = ""
+    var operatorSign = OperatorSign.none
+    var currentPosition = Position.firstNumber
     
-    var firstValueDoubleType: Double {
-        var result: Double = 0
-        var index = firstValue.count - 1
-        for num in firstValue {
-            result += num * pow(10.0, Double(index))
-            index -= 1
+    mutating func appendNumber(_ character: String) {
+        if currentPosition == .firstNumber {
+            firstNumber += character
+        } else if currentPosition == .signOperator {
+            secondNumber += character
+            currentPosition = .secondNumber
+        } else if currentPosition == .secondNumber {
+            secondNumber += character
         }
-        return result
     }
     
-    var secondValueDoubleType: Double {
-        var result: Double = 0
-        var index = secondValue.count - 1
-        for num in secondValue {
-            result += num * pow(10.0, Double(index))
-            index -= 1
+    func screanText() -> String {
+        switch currentPosition {
+        case .firstNumber:
+            return firstNumber
+        case .secondNumber:
+            return secondNumber
+        case .signOperator:
+            return operatorSign.text
         }
-        return result
     }
     
-    var solveExpression: Double {
-        switch operationSign {
-        case "+":
-            return firstValueDoubleType + secondValueDoubleType
-        case "-":
-            return firstValueDoubleType - secondValueDoubleType
-        case "*":
-            return firstValueDoubleType * secondValueDoubleType
-        case "/":
-            return firstValueDoubleType / secondValueDoubleType
-        default:
-            return 0
-        }
-        //if operationSign == "+" {
-        //    return firstValueDoubleType + secondValueDoubleType
-        //} else if operationSign == "*" {
-        //    return firstValueDoubleType * secondValueDoubleType
-        //} else if operationSign == "/" {
-        //    return firstValueDoubleType / secondValueDoubleType
-        //} else if operationSign == "-" {
-        //    return firstValueDoubleType - secondValueDoubleType
-        //} else {
-        //    return 0
-        //}
+    mutating func addOperatorSign(_ newOperatorSign: OperatorSign) {
+        operatorSign = newOperatorSign
+        currentPosition = .signOperator
     }
     
+    mutating func calculation() {
+        var resultOfCalculation: Double
+        
+        switch operatorSign {
+        case .multiplication:
+            resultOfCalculation = Double(firstNumber)! * Double(secondNumber)!
+        case .subtraction:
+            resultOfCalculation = Double(firstNumber)! - Double(secondNumber)!
+        case .division:
+            resultOfCalculation = Double(firstNumber)! / Double(secondNumber)!
+        case .addition:
+            resultOfCalculation = Double(firstNumber)! + Double(secondNumber)!
+        case .none:
+            resultOfCalculation = 0
+        }
+
+        firstNumber = "\(resultOfCalculation)"
+        secondNumber = ""
+        operatorSign = .none
+        currentPosition = .firstNumber
+    }
+    
+    
+//    var firstValueDoubleType: Double {
+//        var result: Double = 0
+//        var index = firstValue.count - 1
+//        for num in firstValue {
+//            result += num * pow(10.0, Double(index))
+//            index -= 1
+//        }
+//        return result
+//    }
+    
+//    var secondValueDoubleType: Double {
+//        var result: Double = 0
+//        var index = secondValue.count - 1
+//        for num in secondValue {
+//            result += num * pow(10.0, Double(index))
+//            index -= 1
+//        }
+//        return result
+//    }
+    
+//    var solveExpression: Double {
+//        switch operationSign {
+//        case "+":
+//            return firstValueDoubleType + secondValueDoubleType
+//        case "-":
+//            return firstValueDoubleType - secondValueDoubleType
+//        case "*":
+//            return firstValueDoubleType * secondValueDoubleType
+//        case "/":
+//            return firstValueDoubleType / secondValueDoubleType
+//        default:
+//            return 0
+//        }
+//    }
 }
--- Caclulator/ViewController.swift
+++ Caclulator/ViewController.swift
@@ -9,59 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var calculator = Calculator()
 
     override func viewDidLoad() {
-        print("hello")
         super.viewDidLoad()
         labelOfAction.textColor = .black
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    var expression = Expression(firstValue: [], secondValue: [], operationSign: "none", currentPosition: false)
-    
- 
-
-// FUNCTIONS -----------------------------------------
-    func clearValues(_ expressionGet: Expression) -> Expression {
-        var expression = expressionGet
-        expression.firstValue.removeAll()
-        expression.secondValue.removeAll()
-        expression.currentPosition = false
-        expression.operationSign = ""
-        return expression
     }
 
-    func changeValue(of expressionGet: Expression, to newNum: Double) -> Expression {
-        var expression = expressionGet
-        if expression.currentPosition == false {
-            expression.firstValue.append(newNum)
-        } else {
-            expression.secondValue.append(newNum)
-        }
-        return expression
-    }
-
-    func changeOperationSign(of expressionGet: Expression, to operationSign: String) -> Expression {
-        var expression = expressionGet
-        expression.operationSign = operationSign
-        expression.currentPosition = true
-        labelOfAction.text = operationSign
-        return expression
-    }
-    
-    func changeTextOfLabel() {
-        var textOfLabel = ""
-        if expression.currentPosition == false {
-            for num in expression.firstValue {
-                textOfLabel.append(String(Int(num)))
-            }
-        } else {
-            for num in expression.secondValue {
-                textOfLabel.append(String(Int(num)))
-            }
-        }
-        labelOfAction.text = textOfLabel
+    func updateLabel() {
+        labelOfAction.text = calculator.screanText()
     }
     
     
@@ -71,74 +28,54 @@ class ViewController: UIViewController {
     
 // DECLARATION BUTTONS----------------------------------
     @IBAction func buttonNumOne(_ sender: UIButton) {
-        expression = changeValue(of: expression, to: 1)
-        changeTextOfLabel()
+        calculator.appendNumber("1")
+        updateLabel()
     }
     
     @IBAction func buttonNumTwo(_ sender: UIButton) {
-        expression = changeValue(of: expression, to: 2)
-        changeTextOfLabel()
     }
     
     @IBAction func buttonNumThree(_ sender: UIButton) {
-        expression = changeValue(of: expression, to: 3)
-        changeTextOfLabel()
     }
     
     @IBAction func buttonNumFour(_ sender: UIButton) {
-        expression = changeValue(of: expression, to: 4)
-        changeTextOfLabel()
     }
     
     @IBAction func buttonNUmFive(_ sender: UIButton) {
-        expression = changeValue(of: expression, to: 5)
-        changeTextOfLabel()
     }
     
     @IBAction func buttonNumSix(_ sender: UIButton) {
-        expression = changeValue(of: expression, to: 6)
-        changeTextOfLabel()
     }
     
     @IBAction func buttonNumSeven(_ sender: UIButton) {
-        expression = changeValue(of: expression, to: 7)
-        changeTextOfLabel()
     }
     
     @IBAction func buttonNumEight(_ sender: UIButton) {
-        expression = changeValue(of: expression, to: 8)
-        changeTextOfLabel()
     }
     
     @IBAction func buttonNumNine(_ sender: UIButton) {
-        expression = changeValue(of: expression, to: 9)
-        changeTextOfLabel()
     }
     
     @IBAction func buttonNumZero(_ sender: UIButton) {
-        expression = changeValue(of: expression, to: 0)
-        changeTextOfLabel()
     }
     
     @IBAction func buttonEqual(_ sender: UIButton) {
-        labelOfAction.text = String(expression.solveExpression)
-        expression = clearValues(expression)
+        calculator.calculation()
+        updateLabel()
     }
     
     @IBAction func buttonPlus(_ sender: UIButton) {
-        expression = changeOperationSign(of: expression, to: "+")
+        calculator.addOperatorSign(.addition)
+        updateLabel()
     }
     
     @IBAction func buttonMultiplication(_ sender: UIButton) {
-        expression = changeOperationSign(of: expression, to: "*")
     }
     
     @IBAction func buttonSubtraction(_ sender: UIButton) {
-        expression = changeOperationSign(of: expression, to: "-")
     }
     
     @IBAction func buttonDivision(_ sender: UIButton) {
-        expression = changeOperationSign(of: expression, to: "/")
     }
     
     
